91b0f4e9c8df4594f579c63eca925018d6fd1777
ChatViewsource <chatviewsupporyou@gmail.com>
2025-06-19 21:45:17 +0100

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF7289DA);
+  static const secondary = Color(0xFF99AAB5);
+  static const background = Color(0xFF36393F);
+}
